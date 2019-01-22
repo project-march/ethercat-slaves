2601729b29694e965aaa6a9e852c569fb09b84c8
martijnvandermarel <martijn.vander.marel@hetnet.nl>
2019-01-22 13:49:44 +0100

    more comments

--- ges/Example_GES/src/main.cpp
+++ ges/Example_GES/src/main.cpp
@@ -4,16 +4,19 @@
 #include "Ethercat.h"
 
 #define WAIT_TIME (2) // seconds
-#define APP_TITLE "Example GES"
-#define PC_BAUDRATE (128000)
+#define APP_TITLE "Example GES" // Application name to be printed to terminal
+#define PC_BAUDRATE (128000) // per second
 
+// Easy access to PDOs. Needs to be changed if different PDOs are used
 #define miso            Ethercat::pdoTx.Struct.miso
 #define mosi            Ethercat::pdoRx.Struct.mosi
 
 // LED on DieBieSlave, for testing communication
 DigitalOut statusLed(DBS_LED);
+
 // Serial communication with the pc for debugging
 Serial pc(DBS_UART_USB_TX, DBS_UART_USB_RX, PC_BAUDRATE);
+
 // Ethercat communication with master
 Ethercat ecat(DBS_ECAT_MOSI, DBS_ECAT_MISO, DBS_ECAT_SCK, DBS_ECAT_NCS);
 
@@ -24,8 +27,8 @@ int main() {
   statusLed = 1;
   wait(WAIT_TIME);
   statusLed = 0;
-
   miso.LED_ack = 0;
+
   while(1) {
     // Update the EtherCAT buffer
     ecat.update();
